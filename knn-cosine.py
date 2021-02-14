a843faeedf30db14b4b619d2b2e3d8112ec26c8e
nisa <nisa@local.fr>
2021-02-14 13:58:58 +0700

    restaurant list

--- knn-cosine.py
+++ knn-cosine.py
@@ -3,6 +3,11 @@ import plotly.offline as py
 import plotly.graph_objs as go
 import plotly.tools as tls
 import plotly.express as px
+from textblob import TextBlob
+# Visualisation
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(style="whitegrid", context='talk')
 
 businesses = pd.read_csv('../yelp/yelp_academic_dataset_business.csv', nrows=10000)
 reviews = pd.read_csv('../yelp/yelp_academic_dataset_review.csv', nrows=10000)
@@ -32,7 +37,7 @@ data['sentiment'] = data['rating'].apply(lambda rating: +1 if rating > 3 else -1
 
 
 # split df - positive and negative sentiment:
-positive = data[data['sentiment'] == 1]
+positive = data[data['sentiment'] == +1]
 negative = data[data['sentiment'] == -1]
 
 # print(positive.head(10))
@@ -51,39 +56,72 @@ def remove_punctation(text):
     return final
 
 data['text'] = data['text'].apply(remove_punctation)
-print(data.head(5))
+data[['polarity', 'subjectivity']] = data['text'].apply(lambda x:TextBlob(x).sentiment).to_list()
+# print(data.nlargest(5, ['polarity']))
+# print(data.nsmallest(5, ['polarity']))
+data = data.nlargest(500, ['polarity'])
+
+for var in ['polarity', 'subjectivity']:
+    plt.figure(figsize=(12,4))
+    sns.histplot(data.query("sentiment==1")[var], bins=30, kde=False,
+                 color='green', label='Positive')
+    sns.histplot(data.query("sentiment==-1")[var], bins=30, kde=False,
+                 color='red', label='Negative')
+    plt.legend()
+    plt.title(f'Histogram of {var} by true sentiment');
+    # plt.show()
 
 
 
+data = Dataset.load_from_df(data[['user_id', 'business_id', 'rating']], reader)
 
+from collections import defaultdict
+from surprise import SVD
+from surprise import accuracy
+from surprise.model_selection import KFold
+from surprise import KNNBasic, KNNBaseline
 
-# data = Dataset.load_from_df(combined_business_data[['user_id', 'business_id', 'rating']], reader)
-#
-#
-#
-# from collections import defaultdict
-# from surprise import SVD
-# from surprise import accuracy
-# from surprise.model_selection import KFold
-# from surprise import KNNBasic, KNNBaseline
-#
-# from surprise.model_selection.split import train_test_split
-# trainset, testset = train_test_split(data, test_size=0.33)
-#
-# sim_options = {'name': 'cosine',
-#                'user_based': False
-#                }
-# algo = KNNBasic(k=5, sim_options=sim_options)
+from surprise.model_selection.split import train_test_split
+trainset, testset = train_test_split(data, test_size=0.33)
 
-# sim_options = {'name': 'pearson_baseline',
-#                'min_support': 5,
-#                'user_based': False}
-# base13 = KNNBaseline(k=21,sim_options=sim_options)
+sim_options = {'name': 'cosine',
+               'user_based': True
+               }
+algo = KNNBaseline(k=5, sim_options=sim_options)
+dataUnique = combined_business_data.drop_duplicates(subset="name")
+predictions = algo.fit(trainset).test(testset)
+accuracy.rmse(predictions)
+result = pd.DataFrame(predictions)
+result.drop(columns = {'details'}, inplace = True)
+# predictions = predictions.nlargest(3,  ['r_ui'])
+print(result.head())
+tampil = pd.Series(dataUnique.index)
+# for r in result:
+#     print(r[r['uid']])
 
+# result = result.join(dataUnique,on='iid')
 
-# predictions = algo.fit(trainset).test(testset)
-# accuracy.rmse(predictions)
-# result = pd.DataFrame(predictions)
-# result.drop(columns = {'details'}, inplace = True)
-# print(result.head())
+def get_top_n(predictions, restaurant):
+    """Return the top-N recommendation for each user from a set of predictions.
+
+    Args:
+        predictions(list of Prediction objects): The list of predictions, as
+            returned by the test method of an algorithm.
+        n(int): The number of recommendation to output for each user. Default
+            is 10.
+
+    Returns:
+    A dict where keys are user (raw) ids and values are lists of tuples:
+        [(raw item id, rating estimation), ...] of size n.
+    """
+
+    # First map the predictions to each user.
+    top_n = []
+    i = 10
+    for uid, iid, true_r, est, _ in predictions:
+        top_n.append(dataUnique[dataUnique['business_id'] == iid])
+    print(top_n)
+
+get_top_n(predictions, dataUnique)
+
 
